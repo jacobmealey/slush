c048c4864d947afe4d7ac5defbf9d6279d886d9a
Jacob Mealey <jacobmealey.maine@gmail.com>
2025-01-27 20:09:36 -0500

    track change_dir

---
+++ src/expr/change_dir.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::path::Path;
+pub struct ChangeDir {
+    path: Box<Path>
+}
+impl ChangeDir {
+    pub fn eval(&self) -> i32 {
+        match env::set_current_dir(&self.path) {
+            Ok(()) => 0,
+            Err(_) => 1
+        }
+    }
+
+    pub fn new(path: &str) -> ChangeDir {
+        ChangeDir{ path: Path::new(path).into() }
+    }
+}
