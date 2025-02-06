295402ea49198b7a9051448c8b116b3810c2daec
Jacob Mealey <jacobmealey.maine@gmail.com>
2025-02-06 07:57:51 -0500

    add clean_path library

--- src/expr/change_dir.rs
+++ src/expr/change_dir.rs
@@ -1,10 +1,20 @@
+use clean_path::Clean; // crate for 'cleaning' paths, turns some/path//../other_path into
+                       // some/other_path, used for setting PWD
 use std::env;
 use std::path::Path;
+
 pub struct ChangeDir {
     path: Box<Path>,
 }
 impl ChangeDir {
     pub fn eval(&self) -> i32 {
+        env::set_var(
+            "PWD",
+            Path::new(&env::var("PWD").unwrap_or("".to_string()))
+                .join(&self.path)
+                .clean()
+                .as_os_str(),
+        );
         match env::set_current_dir(&self.path) {
             Ok(()) => 0,
             Err(_) => 1,
